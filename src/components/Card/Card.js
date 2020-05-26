ad43e2256e02ece7c72909804ae7a199c3841196
Arlune <morgane.gaillard69@gmail.com>
2020-05-26 11:52:00 +0200

    Add Card Component & storie

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Card.scss'
+
+function Card({ children }) {
+  return <div>{children}</div>
+}
+
+export default Card
+
+/* 
+Une carte qui reçoit (children) ce que l'on veut : une image (fit content) et/ou du texte
+*/
